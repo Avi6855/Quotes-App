96330ba4e3dfc9ba8bfbb811970999547a069642
Avi6855 <avinashpatil6855@gmail.com>
2024-02-09 23:00:16 +0530

    Quotes Android Application

---
+++ app/src/main/java/com/avinashpatil/quotes/QuoteApiService.java
@@ -0,0 +1,9 @@
+package com.avinashpatil.quotes;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface QuoteApiService {
+    @GET("random")
+    Call<QuoteResponse> getRandomQuote();
+}
---
+++ app/src/main/java/com/avinashpatil/quotes/QuoteResponse.java
@@ -0,0 +1,14 @@
+package com.avinashpatil.quotes;
+
+public class QuoteResponse {
+    private String content;
+    private String author;
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
